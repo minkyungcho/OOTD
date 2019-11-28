7a9a5c3ba61cfb05aa0517135ce612c5a7701754
ming_Mac <xiuperjjo@naver.com>
2019-11-28 23:54:58 +0900

    add OOTD crawling

--- data/brandi/urls.py
+++ data/brandi/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 from . import views as brandi_views
 
 urlpatterns = [
-    path('', brandi_views.index, name="brandies"),
+    path('', brandi_views.index, name="index"),
+    path('link/<int:category_id>/', brandi_views.link, name="link"),
+    path('product/<int:product_id>/', brandi_views.product, name="product"),
 ]
     
--- data/brandi/views.py
+++ data/brandi/views.py
@@ -7,23 +7,30 @@ from bs4 import BeautifulSoup
 
 # Create your views here.
 def index(request):
-    url = 'https://cf-api-c.brandi.me/v1/web/categories/3/products?offset=0&limit=100&order=popular&type=all&order=popular'
+
+    return render(request, 'index.html')
+
+def link(request, category_id):
+
+    # url = 'https://cf-api-c.brandi.me/v1/web/categories/3/products?offset=0&limit=100&order=popular&type=all&order=popular'
+    url = f'https://cf-api-c.brandi.me/v1/web/categories/{category_id}/products?offset=0&limit=100&order=popular&type=all&order=popular'
+
     headers = {
-        'authority': 'cf-api-c.brandi.me',
-        'method': 'GET',
-        'path': '/v1/web/categories/3/products?offset=0&limit=100&order=popular&type=all&order=popular',
-        'scheme': 'https',
-        'accept': 'application/json, text/plain, */*',
-        'accept-encoding': 'gzip, deflate, br',
-        'accept-language': 'ko-KR,ko;q=0.9,en-US;q=0.8,en;q=0.7',
+        # 'authority': 'cf-api-c.brandi.me',
+        # 'method': 'GET',
+        # 'path': '/v1/web/categories/3/products?offset=0&limit=100&order=popular&type=all&order=popular',
+        # 'scheme': 'https',
+        # 'accept': 'application/json, text/plain, */*',
+        # 'accept-encoding': 'gzip, deflate, br',
+        # 'accept-language': 'ko-KR,ko;q=0.9,en-US;q=0.8,en;q=0.7',
         'authorization': '3b17176f2eb5fdffb9bafdcc3e4bc192b013813caddccd0aad20c23ed272f076_1423639497',
-        'content-type': 'application/x-www-form-urlencoded; charset=utf-8',
-        'cookie': 'PHPSESSID=s10jgtah1vq3v4kju4t39hpqut',
-        'origin': 'https://www.brandi.co.kr',
-        'referer': 'https://www.brandi.co.kr/categories/3',
-        'sec-fetch-mode': 'cors',
-        'sec-fetch-site': 'cross-site',
-        'User-Agent' : 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.97 Safari/537.36'
+        # 'content-type': 'application/x-www-form-urlencoded; charset=utf-8',
+        # 'cookie': 'PHPSESSID=s10jgtah1vq3v4kju4t39hpqut',
+        # 'origin': 'https://www.brandi.co.kr',
+        # 'referer': 'https://www.brandi.co.kr/categories/3',
+        # 'sec-fetch-mode': 'cors',
+        # 'sec-fetch-site': 'cross-site',
+        # 'User-Agent' : 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.97 Safari/537.36'
     }
     response = requests.get(url, headers=headers)
     print(response)
@@ -33,8 +40,66 @@ def index(request):
     dics = data['data']
     for dic in dics:
         idd = dic['id']
-        print(idd)
+        # print(idd)
+        name = dic['name']
+        # print(name)
     context = {
         'dics': dics
+        # 'product_id': dic
     }
-    return render(request, 'index.html', context)
+    return render(request, 'link.html', context)
+
+def product(request, product_id):
+    url = f'https://cf-api-c.brandi.me/v1/web/products/{product_id}?res-type=section1'
+    # url = 'https://cf-api-c.brandi.me/v1/web/products/11339428?res-type=section1'
+    headers = {
+        'authorization': '3b17176f2eb5fdffb9bafdcc3e4bc192b013813caddccd0aad20c23ed272f076_1423639497'
+    }
+    response = requests.get(url, headers=headers)
+    print(response)
+    
+    data = response.json()
+    # products = data[]
+    # print(data["data"]["category_hierarchies"])
+    category_hierarchies = data["data"]["category_hierarchies"]
+    options = data["data"]["options"]
+    colors = []
+    if len(options) > 1:
+        for option in options:
+            if len(option["attributes"]) > 1:
+                a = option["attributes"][0]["value"]
+                print(a)
+                if a in colors:
+                    continue
+                colors.append(a)        
+    
+    print(colors)
+        # attributes = data["data"]["options"][0]["attributes"]
+    # attributes = data["data"]["options"][0]["attributes"]
+    # for attr in attributes:
+        # print(attr)
+    category = category_hierarchies[0]['name']
+    length = len(category_hierarchies)
+    if length == 2:
+        category_type = category_hierarchies[1]['name']
+    else:
+        category_type = category
+    # print(category+" "+category_type+" "+str(length))
+    
+
+    products = []
+    for color in colors:
+        temp = {
+            "id": product_id,
+            "category": category,
+            "category_type": category_type,
+            "color": color
+        }
+        products.append(temp)
+    
+    context = {
+        'products': products
+    }
+    
+    return render(request, 'product.html', context)
+    # return render(request, 'product.html', temp)

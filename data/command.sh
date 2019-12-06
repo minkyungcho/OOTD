a4a041ee0be897bd6383bd4a485081327ef4b1b5
ming_Mac <xiuperjjo@naver.com>
2019-12-06 17:56:09 +0900

    change views.py & add gitignore db

--- data/command.sh
+++ data/command.sh
@@ -39,6 +39,8 @@ m11.save()
 m12 = Month(month="12")
 m12.save()
 
+t1 = Temp(temp="0")
+t1.save()
 t1 = Temp(temp="1")
 t2 = Temp(temp="2")
 t3 = Temp(temp="3")
@@ -85,3 +87,8 @@ c = Cloth.objects.filter(label="1T00FF5", product_id=12331893).update(color="화
 
 # read cloth
 Cloth.objects.filter(cloth_type="후드/맨투맨")
+
+# update temp
+t = Temp.objects.filter(temp=0).update(temp=9)
+
+git rm -rf --cached *.pyc

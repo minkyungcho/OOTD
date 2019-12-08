842409003487e72d655dd83c5172ff67ae3ecefb
An-Sohyoun <ansoh94@naver.com>
2019-12-08 18:11:35 +0900

    create Sign Up and Sign In

--- OOTDweb/ootd/settings.py
+++ OOTDweb/ootd/settings.py
@@ -32,6 +32,8 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'codi',
+    'accounts',
+    'bootstrap4',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',

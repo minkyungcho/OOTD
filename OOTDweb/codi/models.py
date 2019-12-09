680a69e3834a83b3f2c5303f251e7d159a3d0574
JYn0 <j_y__n@naver.com>
2019-12-09 17:54:28 +0900

    codiBook only write

--- OOTDweb/codi/models.py
+++ OOTDweb/codi/models.py
@@ -3,7 +3,6 @@ from django.conf import settings
 from django.contrib.auth.models import User
 from imagekit.models import ImageSpecField, ProcessedImageField
 from imagekit.processors import ResizeToFill, Thumbnail
-
 # Create your models here.
 
 class Category(models.Model):
@@ -34,3 +33,31 @@ class Closet(models.Model):
     updated_at = models.DateTimeField(auto_now=True)
     clothes = models.ManyToManyField(Cloth, related_name="closets")
 
+
+class Article(models.Model):
+    contents = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    
+    # def comments(self):
+    #     # article_id가 self.id인 것을 return해라
+    #     return Comment.objects.filter(article_id=self.id)
+    # def article_images(self):
+    #     return ArticleImages.objects.filter(article_id=self.id)
+
+class Board(models.Model):
+    contents = models.CharField(max_length=16)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+# class Comment(models.Model):
+#     # Article 하나가 여러개의 Comment를 갖는다(1:N)
+#     contents = models.TextField()
+#     created_at = models.DateTimeField(auto_now_add=True)
+#     updated_at = models.DateTimeField(auto_now=True)
+#     article = models.ForeignKey(Article, on_delete=models.CASCADE)
+#     # on_delete:옵션, CASCADE -> 게시글이 삭제되면 댓글도 삭제되어야함
+
+# class HashTag(models.Model):
+#     tag = models.CharField(max_length=16, unique=True)
+#     articles = models.ManyToManyField(Article, related_name="tags")

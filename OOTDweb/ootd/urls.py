c1ff57bd86bf300c8b309e9361b994d58a6cedf2
ming_Mac <xiuperjjo@naver.com>
2019-12-12 16:37:02 +0900

    add colorThief

--- OOTDweb/ootd/urls.py
+++ OOTDweb/ootd/urls.py
@@ -22,6 +22,8 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('codi/', include('codi.urls')),
     path('accounts/', include('accounts.urls')),
+    path('colorthief/', include('colorThief.urls')),
+
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
